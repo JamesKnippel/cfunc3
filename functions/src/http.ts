ee2390d1f949aed27b68c10dc62e44d85032d86a
JamesKnippel <Jimmyknippel@gmail.com>
2020-03-17 11:12:11 -0700

    added wildcard refactor and comments for brain refresh

--- functions/src/http.ts
+++ functions/src/http.ts
@@ -5,5 +5,18 @@ admin.initializeApp();
 
 export const basicHTTP = functions.https.onRequest((request, response) => {
 
-    response.send('Hello from Firebase, James!');
+    /* parse the request for data, wildcard in
+    ex: http://localhost:5001/fir-primer-20332/us-central1/
+    basicHTTP + ?name=jimbo%20knippel 
+    where: ?=name is your url parameter, usually $uid in firebase
+    for production
+    */
+    const name = request.query.name;
+    response.send (`hello ${name}`);
+
+    /* HardCode Implementation */
+    // response.send('Hello from Firebase, James!');
+    
+    
+
 })
