05177c826c289c5f5a7eceaded20be2013f9524a
JamesKnippel <Jimmyknippel@gmail.com>
2020-03-17 10:50:49 -0700

    create http function

---
+++ functions/src/http.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+export const basicHTTP = functions.https.onRequest((request, response) => {
+
+    response.send('Hello from Firebase, James!');
+})
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,5 @@
-import * as functions from 'firebase-functions';
+/* Import individual Cloud Functions here, similar to Angular Modules */
+export { basicHTTP } from './http';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
