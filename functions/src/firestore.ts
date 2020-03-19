518f60b4312a50340350333d88c29cf4cea4c39b
JamesKnippel <Jimmyknippel@gmail.com>
2020-03-19 14:39:10 -0700

    function refactor, added onUpdate with examples for testing

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -4,8 +4,8 @@ import * as admin from 'firebase-admin';
 /* Defines Firestore instance */
 const db = admin.firestore();
 
-export const documentCounter = functions.firestore
-    .document('documents/estateManagement/Wills/{documentId}')
+export const gameCounter = functions.firestore
+    .document('games/{gameId}')
     .onCreate(async (snapshot, context) => {
         const data = snapshot.data();
         const userRef = db.doc(`users/${data.uid}`);
@@ -14,5 +14,37 @@ export const documentCounter = functions.firestore
 
         return userRef.update({
             updateCount: userData.updateCount + 1
+        });
+    });
+
+    /* onUpdate takes two arguments for testing. expects: 'oh baby a triple'
+    userTrend({ before: {
+        uid: 'bradley',
+        score: 777
+    }, after: {
+        uid: 'bradley',
+        score: 9001
+    }})
+    */
+export const userTrend = functions.firestore
+    .document('games/{gameId}')
+    .onUpdate((snapshot, context) => {
+        const before = snapshot.before.data();
+        const after = snapshot.after.data();
+        let trend;
+
+        /* process logic */
+        if (after.score >= before.score) {
+            trend = 'Oh baby, a triple!';
+        } else {
+            trend = 'oof, git gud kid lmao xDxD';
+        }
+
+        /* Target to send processed logic */
+        const userRef = db.doc(`users/${after.uid}`);
+
+        /* Send processed logic */
+        return userRef.update({
+            trend
         });
     });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 /* Import individual Cloud Functions here, similar to Angular Modules */
 export { basicHTTP, api } from './http';
 export { createUserRecord } from './auth';
-export { documentCounter } from './firestore';
+export { gameCounter, userTrend } from './firestore';
 
 
 
