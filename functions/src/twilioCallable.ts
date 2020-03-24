15552a3970e72a591a28dcd5c5c6b42949f6d341
JamesKnippel <Jimmyknippel@gmail.com>
2020-03-23 20:28:04 -0700

    twilio callable text message backend, needs client side

--- functions/src/twilioCallable.ts
+++ functions/src/twilioCallable.ts
@@ -1,2 +1,29 @@
-import * as twilio from 'twilio';
+import * as Twilio from 'twilio';
+
+import * as functions from 'firebase-functions';
 /* firebase functions:config:set apiName.sid="whatever" apiName.token="tokenCreds" */
+const credentials = functions.config().twilio;
+const client = Twilio(credentials.sid, credentials.token);
+
+import * as admin from 'firebase-admin';
+const db = admin.firestore();
+
+export const sendText = functions.https.onCall(async (data, context) => {
+    /* guarantees user is loggedin on front end */
+    const userId = context.auth.uid;
+
+    const userRef = db.doc(`users/${userId}`);
+
+    const userSnapshot = await userRef.get();
+
+    /* selects index in Firestore with relevant nam - hardcoding for example */
+    const number = userSnapshot.data().phoneNumber;
+
+    return client.messages.create({
+        body: data.message,
+        to: number,
+        from: '+12345678901'
+    })
+})
+
+
